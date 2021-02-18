0588535319f180e1cc20b4c97eede063c2208aaf
P <github@littleurl.org>
2021-02-17 20:46:32 -0500

    Create RaspianBuilder.sh
    code to wrap writing a IMG file for raspberry pi and enabling ssh by default

---
+++ RaspianBuilder.sh
@@ -0,0 +1,18 @@
+#!/bin/sh 
+
+
+dd if=$1 of=$2  bs=4096 status=progress 
+
+ROOT=$2"2"
+BOOT=$2"1"
+
+/usr/bin/mount $ROOT /mnt
+/usr/bin/mount $BOOT /mnt/boot 
+
+/usr/bin/touch ssh /mnt/boot
+/usr/bin/sync
+
+/usr/bin/umount /mnt/boot
+/usr/bin/umount /mnt
+
+echo "done!" 
